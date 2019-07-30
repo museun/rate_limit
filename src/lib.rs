8cb6f0d60674ffa00abdef7dbb3c81cc5fd363a9
museun <museun@outlook.com>
2019-07-30 09:15:41 -0400

    split sync/unsync out of an enum so Mutex et.al will see T:Send

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
-/// Limiter is a simple token bucket-style rate limiter
+/// SyncLimiter is a simple token bucket-style rate limiter
 ///
 /// When its tokens are exhausted, it will block the current thread until its refilled
 ///
@@ -20,8 +20,9 @@ use std::time::{Duration, Instant};
 /// ```
 /// # fn main() {
 /// # use std::time::Duration;
+/// # use rate_limit::*;
 /// // limits to 3 `.take()` per 1 second
-/// let limiter = Limiter::full(3, Duration::from_secs(1));
+/// let limiter = SyncLimiter::full(3, Duration::from_secs(1));
 /// for _ in 0..10 {
 ///     // every 3 calls within 1 second will cause the next .take() to block
 ///     // so this will take ~3 seconds to run (10 / 3 = ~3)
@@ -29,21 +30,19 @@ use std::time::{Duration, Instant};
 /// }
 ///
 /// // initially empty, it will block for 1 second then block for every 3 calls per 1 second
-/// let limiter = Limiter::empty(3, Duration::from_secs(1));
+/// let limiter = SyncLimiter::empty(3, Duration::from_secs(1));
 /// for _ in 0..10 {
 ///     limiter.take();
 /// }
 /// # }
 /// ```
-///
-/// The `_unsync()` variants create a cheaper, single-threaded form
 #[derive(Clone)]
-pub struct Limiter {
+pub struct SyncLimiter {
     cap: u64,
-    bucket: Bucket,
+    bucket: SyncBucket,
 }
 
-impl Limiter {
+impl SyncLimiter {
     /// Create a new, thread-safe limiter
     ///
     /// `cap` is the number of total tokens available
@@ -54,21 +53,7 @@ impl Limiter {
     pub fn new(cap: u64, initial: u64, period: Duration) -> Self {
         Self {
             cap,
-            bucket: Bucket::sync(cap, initial, period),
-        }
-    }
-
-    /// Create a new, single-threaded limiter
-    ///
-    /// `cap` is the number of total tokens available
-    ///
-    /// `initial` is how many are initially available
-    ///
-    /// `period` is how long it'll take to refill all of the tokens
-    pub fn new_unsync(cap: u64, initial: u64, period: Duration) -> Self {
-        Self {
-            cap,
-            bucket: Bucket::unsync(cap, initial, period),
+            bucket: sync(cap, initial, period),
         }
     }
 
@@ -81,7 +66,7 @@ impl Limiter {
     pub fn full(cap: u64, period: Duration) -> Self {
         Self {
             cap,
-            bucket: Bucket::sync(cap, cap, period),
+            bucket: sync(cap, cap, period),
         }
     }
 
@@ -94,33 +79,7 @@ impl Limiter {
     pub fn empty(cap: u64, period: Duration) -> Self {
         Self {
             cap,
-            bucket: Bucket::sync(cap, 0, period),
-        }
-    }
-
-    /// Create a single-threaded limiter thats pre-filled
-    ///
-    /// `cap` is the number of total tokens available
-    ///
-    /// `period` is how long it'll take to refill all of the tokens
-    #[inline]
-    pub fn full_unsync(cap: u64, period: Duration) -> Self {
-        Self {
-            cap,
-            bucket: Bucket::unsync(cap, cap, period),
-        }
-    }
-
-    /// Create an empty single-threaded limiter
-    ///
-    /// `cap` is the number of total tokens available
-    ///
-    /// `period` is how long it'll take to refill all of the tokens
-    #[inline]
-    pub fn empty_unsync(cap: u64, period: Duration) -> Self {
-        Self {
-            cap,
-            bucket: Bucket::unsync(cap, 0, period),
+            bucket: sync(cap, 0, period),
         }
     }
 
@@ -133,29 +92,135 @@ impl Limiter {
     pub fn consume(&self, tokens: u64) -> Result<u64, Duration> {
         let now = Instant::now();
 
-        // no specialization yet and I don't want to have the consumers pass in
-        // the {Sync|Unsync}Bucket, so simply just use a macro here
-        macro_rules! consume {
-            ($inner:expr) => {{
-                if let Some(n) = $inner.refill(now) {
-                    $inner.tokens = std::cmp::min($inner.tokens + n, self.cap);
-                };
+        let mut bucket = self.bucket.0.lock().unwrap();
+        if let Some(n) = bucket.refill(now) {
+            bucket.tokens = std::cmp::min(bucket.tokens + n, self.cap);
+        };
 
-                if tokens <= $inner.tokens {
-                    $inner.tokens -= tokens;
-                    $inner.backoff = 0;
-                    return Ok($inner.tokens);
-                }
-
-                let prev = $inner.tokens;
-                Err($inner.estimate(tokens - prev, now))
-            }};
+        if tokens <= bucket.tokens {
+            bucket.tokens -= tokens;
+            bucket.backoff = 0;
+            return Ok(bucket.tokens);
         }
 
-        match self.bucket {
-            Bucket::Sync(ref sync) => consume!(sync.lock().unwrap()),
-            Bucket::Unsync(ref unsync) => consume!(unsync.borrow_mut()),
+        let prev = bucket.tokens;
+        Err(bucket.estimate(tokens - prev, now))
+    }
+
+    /// Consumes `tokens` blocking if its trying to consume more than available
+    ///
+    /// Returns how many tokens are available
+    pub fn throttle(&self, tokens: u64) -> u64 {
+        loop {
+            match self.consume(tokens) {
+                Ok(rem) => return rem,
+                Err(time) => std::thread::sleep(time),
+            }
         }
+    }
+
+    /// Take a token, blocking if unavailable
+    ///
+    /// Returns how many tokens are available
+    #[inline]
+    pub fn take(&self) -> u64 {
+        self.throttle(1)
+    }
+}
+
+/// UnsyncLimiter is a simple token bucket-style rate limiter (cheaper to clone, but not thread safe)
+///
+/// When its tokens are exhausted, it will block the current thread until its refilled
+///
+/// The limiter is cheaply-clonable
+/// ```
+/// # fn main() {
+/// # use std::time::Duration;
+/// # use rate_limit::*;
+/// // limits to 3 `.take()` per 1 second
+/// let limiter = UnsyncLimiter::full(3, Duration::from_secs(1));
+/// for _ in 0..10 {
+///     // every 3 calls within 1 second will cause the next .take() to block
+///     // so this will take ~3 seconds to run (10 / 3 = ~3)
+///     limiter.take();
+/// }
+///
+/// // initially empty, it will block for 1 second then block for every 3 calls per 1 second
+/// let limiter = UnsyncLimiter::empty(3, Duration::from_secs(1));
+/// for _ in 0..10 {
+///     limiter.take();
+/// }
+/// # }
+/// ```
+#[derive(Clone)]
+pub struct UnsyncLimiter {
+    cap: u64,
+    bucket: UnsyncBucket,
+}
+
+impl UnsyncLimiter {
+    /// Create a new, thread-safe limiter
+    ///
+    /// `cap` is the number of total tokens available
+    ///
+    /// `initial` is how many are initially available
+    ///
+    /// `period` is how long it'll take to refill all of the tokens
+    pub fn new(cap: u64, initial: u64, period: Duration) -> Self {
+        Self {
+            cap,
+            bucket: unsync(cap, initial, period),
+        }
+    }
+
+    /// Create a thread-safe limiter thats pre-filled
+    ///
+    /// `cap` is the number of total tokens available
+    ///
+    /// `period` is how long it'll take to refill all of the tokens
+    #[inline]
+    pub fn full(cap: u64, period: Duration) -> Self {
+        Self {
+            cap,
+            bucket: unsync(cap, cap, period),
+        }
+    }
+
+    /// Create an empty thread-safe limiter
+    ///
+    /// `cap` is the number of total tokens available
+    ///
+    /// `period` is how long it'll take to refill all of the tokens
+    #[inline]
+    pub fn empty(cap: u64, period: Duration) -> Self {
+        Self {
+            cap,
+            bucket: unsync(cap, 0, period),
+        }
+    }
+
+    /// Tries to consume `tokens`
+    ///
+    /// If it will consume more than available then an Error is returned.
+    /// Otherwise it returns how many tokens are left
+    ///
+    /// This error is the `Duration` of the next available time
+    pub fn consume(&self, tokens: u64) -> Result<u64, Duration> {
+        let now = Instant::now();
+
+        let mut bucket = self.bucket.0.borrow_mut();
+        if let Some(n) = bucket.refill(now) {
+            bucket.tokens = std::cmp::min(bucket.tokens + n, self.cap);
+        };
+
+        if tokens <= bucket.tokens {
+            bucket.tokens -= tokens;
+            bucket.backoff = 0;
+            return Ok(bucket.tokens);
+        }
+
+        let prev = bucket.tokens;
+        Err(bucket.estimate(tokens - prev, now))
     }
 
     /// Consumes `tokens` blocking if its trying to consume more than available
@@ -180,18 +245,17 @@ impl Limiter {
 }
 
 #[derive(Clone)]
-enum Bucket {
-    Sync(Arc<Mutex<Inner>>),
-    Unsync(Rc<RefCell<Inner>>),
+struct SyncBucket(Arc<Mutex<Inner>>);
+
+#[derive(Clone)]
+struct UnsyncBucket(Rc<RefCell<Inner>>);
+
+fn sync(cap: u64, initial: u64, period: Duration) -> SyncBucket {
+    SyncBucket(Arc::new(Mutex::new(Inner::new(cap, initial, period))))
 }
 
-impl Bucket {
-    fn sync(cap: u64, initial: u64, period: Duration) -> Self {
-        Bucket::Sync(Arc::new(Mutex::new(Inner::new(cap, initial, period))))
-    }
-    fn unsync(cap: u64, initial: u64, period: Duration) -> Self {
-        Bucket::Unsync(Rc::new(RefCell::new(Inner::new(cap, initial, period))))
-    }
+fn unsync(cap: u64, initial: u64, period: Duration) -> UnsyncBucket {
+    UnsyncBucket(Rc::new(RefCell::new(Inner::new(cap, initial, period))))
 }
 
 #[derive(Clone)]
